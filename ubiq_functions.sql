cb38297915a7ab88ed1c78a37fd6ddffa26cc563
Sam Craig <sam@transeosolutions.com>
2023-03-29 14:35:34 -1000

    Fix ubiq_cred-using session begin

--- ubiq_functions.sql
+++ ubiq_functions.sql
@@ -70,18 +70,20 @@ drop table ubiq_ffs_cache;
 create or replace procedure ubiq_begin_fpe_session("ffs_names" varchar)
 returns varchar
 language javascript
-execute as owner
 as
 $$
     var sql = `create or replace temporary table ubiq_ffs_cache (cache object) as 
-    select _ubiq_broker_fetch_ffs_and_fpe_key( 
-        '${ffs_names}', 
-        (select access_key_id from ubiq_creds), 
-        (select secret_signing_key from ubiq_creds)
-    );`
-
+        select _ubiq_python_fetch_data_key(
+            '${ffs_names}',
+            (select secret_crypto_access_key from ubiq_creds),
+            (select _ubiq_broker_fetch_ffs_and_fpe_key( 
+                '${ffs_names}', 
+                (select access_key_id from ubiq_creds), 
+                (select secret_signing_key from ubiq_creds)
+            ))
+        );`
     try {
-        snowflake.execute({sqltext: sql});
+        snowflake.execute({sqlText: sql});
         return "succeeded"
     }
     catch (err) {
@@ -89,43 +91,21 @@ $$
     }
 $$;
 
--- Creates Cache without Reliance on Ubiq_creds table
--- Will still require keys to be unwrapped using above functions for use
-CREATE OR REPLACE PROCEDURE UBIQ_BEGIN_FPE_SESSION("FFS_NAMES" VARCHAR(16777216), "ACCESS_KEY" VARCHAR(24), "SECRET_SIGNING_KEY" VARCHAR(44))
-RETURNS VARCHAR(16777216)
-LANGUAGE JAVASCRIPT
-EXECUTE AS OWNER
-AS
-$$
-    var sql = `create or replace temporary table ubiq_ffs_cache (cache object) as 
-    SELECT _ubiq_broker_fetch_ffs_and_fpe_key( 
-        '${FFS_NAMES}', 
-        '${ACCESS_KEY}', 
-        '${SECRET_SIGNING_KEY}'
-    );`
-    try {
-        snowflake.execute({sqlText: sql});
-        return "Succeeded"
-    }
-    catch (err) {
-        return "Failed: " + err;
-    }
-$$;
 
 -- Creates Cache with unwrapped keys; no Secret Crypto Key needed for enc/dec fpe cache functions.
-CREATE OR REPLACE PROCEDURE UBIQ_BEGIN_FPE_SESSION("FFS_NAME" VARCHAR(16777216), "ACCESS_KEY" VARCHAR(24), "SECRET_SIGNING_KEY" VARCHAR(44), "SECRET_CRYPTO_ACCESS_KEY" VARCHAR(44))
-RETURNS VARCHAR(16777216)
-LANGUAGE JAVASCRIPT
-AS
+create or replace procedure ubiq_begin_fpe_session("ffs_name", "access_key", "secret_signing_key", "secret_crypto_access_key")
+returns varchar
+language javascript
+as
 $$
     var sql = `create or replace temporary table ubiq_ffs_cache (cache object) as 
         select _ubiq_python_fetch_data_key(
-            '${FFS_NAME}',
-            '${SECRET_CRYPTO_ACCESS_KEY}',
+            '${ffs_name}',
+            '${secret_crypto_access_key}',
             (select _ubiq_broker_fetch_ffs_and_fpe_key( 
-                '${FFS_NAME}',
-                '${ACCESS_KEY}', 
-                '${SECRET_SIGNING_KEY}'
+                '${ffs_name}',
+                '${access_key}', 
+                '${secret_signing_key}'
             ))
         );`
     try {
